49ee79883ce4d4e76762cc1606cd4aedc93ad4a3
MRTBSKTP <45196508+MRTBSKTP@users.noreply.github.com>
2019-03-28 23:54:55 +0300

    File showing JAVA 8 streams API use

---
+++ Java/StreamsAPIEdu.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.List;
+
+/**
+ *  About streams of JAVA8 ...
+ *  
+ *    We can transform a collection to a stream with .stream() method
+ *  And turn it back to a collection with .collect() method
+ * 
+ *    Using streams makes manipulating collections easier.
+ *  When a collection is turned into a stream, called methods on this 
+ *  stream are applied element-wise this enables omitting loops to manipulate
+ *  collection data 
+ *
+ *  Visualization:
+ *  
+ *  - List to stream
+ *  List < [1, 2, 3, 4,.....] > .stream() ---> stream [1-2-3-4-....] .#ElementWiseMethods()
+ *  
+ *  - Stream to list
+ *  stream [1-2-3-4-....] .collect(.toList()) ---> List < [1, 2, 3, 4,.....] >
+ * 
+ *     Operations on streams are divided in 2 parts
+ *     1 - Intermediate operations : These return stream objects so a chain of these 
+ *                                  operations can be applied e.g. ;
+ *                                  #####.stream().filter().map().......
+ *
+ *     2 - Terminal operations     : These return non stream objects so calling stream
+ *                                  methods beyond these are invalid e.g. ;
+ *                                  #####.stream().foreach(); -> no stream beyond foreach
+ * 
+ *     Output operations are only handled when a terminal function exists in a operation pipeline !
+ * 
+ *  Mert Beşiktepe 2019 ITU
+ */
+public class StreamsAPIEdu {
+
+    /**
+     * Some applications about streams introduced in Java 8
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /* 1 - Conversion between streams and collections */
+
+        // can only collect objects. So in case of primitive types you must
+        // call boxed() method to wrap them in to wrapper classes like Integer
+        List<Integer> listInt = IntStream.range(1, 7).boxed().collect(Collectors.toList()); 
+
+        // print list with lambda expression
+        // this is like saying :
+        // for each integer in listInt call .println()
+        listInt.forEach(integer -> System.out.println(integer));
+        
+        /* 2 - Filtering elements */
+
+        // filtering is applied based on a rule called 'predicate' 
+        // first task is creating a predicate
+        List<String> list1 = Arrays.asList("Kelime1","Kelime2","Kelime3");
+        
+        // turn it to a stream, filter and collect back into antother list this time
+        List<String> result = list1.stream()
+                              // filter the elements that not ending with "2"
+                              .filter(kelime -> !kelime.endsWith("2"))
+                              .collect(Collectors.toList());
+        
+        // again a lambda a expression but this time 
+        // with '::' as method reference operator.
+        // this is like saying
+        // go to System.out class and call println on each element
+        result.forEach(System.out::println);
+        
+        /* 3 - Mapping elements to functions */
+
+        // map() method enables mapping streams to functions element-wise
+        // create a list, stream it filter and map the filtered ones to a function.
+        List<String> list2 = Arrays.asList("mert","Java","tutorial");
+
+        // stream filter map operations back to back
+        // mutated value of stream will be vanished if it is not referenced
+        list2.stream()
+             .filter(entry -> entry.equals("mert") || entry.equals("tutorial"))
+             // map the filtered stream to uppercase function
+             .map(entry -> entry.toUpperCase())
+             // then print it with for each
+             .forEach(System.out::println);
+
+        // No changes applied to object because stream wasn't referenced
+        list2.forEach(System.out::println);
+        
+        // mapping to an arbitrary lambda function
+        list2.stream()
+             // lambda function defined in "{}"
+             .map(kelime -> {
+                             int index = 0;
+                             // this is redundant only to show we can define such things here.
+                             String draft = null;
+                             draft = kelime;
+                             for (char harf : draft.toCharArray()) {
+                                 System.out.format("++++ %c ++++",kelime.charAt(index));
+                                 index += 1;
+                                }
+                            return kelime;
+                            } )
+             // calling a terminal function to see the results
+             .forEach(System.out::println);
+        
+    }
+}
